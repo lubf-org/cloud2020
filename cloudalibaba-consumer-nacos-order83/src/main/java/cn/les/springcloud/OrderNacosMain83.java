2c34d41c9fa867ef843153fc24ef71924890f2f3
andyleisure <lubingfeng88@gmail.com>
2020-12-22 18:56:18 +0800

    新增nacos为注册中心

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/cn/les/springcloud/OrderNacosMain83.java
@@ -0,0 +1,33 @@
+package cn.les.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author
+ * @version v1.0.0
+ * @descripton
+ * @2020/12/22 16:57
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderNacosMain83
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
+    }
+
+}
+
---
+++ cloudalibaba-provider-payment9001/src/main/java/cn/les/springcloud/PaymentMain9001.java
@@ -0,0 +1,20 @@
+package cn.les.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author
+ * @version v1.0.0
+ * @descripton
+ * @2020/12/22 16:19
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+
+}
