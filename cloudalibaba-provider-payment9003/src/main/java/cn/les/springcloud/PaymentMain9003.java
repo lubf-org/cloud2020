3bf0b6b74c6a345deb4625e2ce9ed20ca72e2c56
andyleisure <lubingfeng88@gmail.com>
2020-12-28 11:18:32 +0800

    sentinel整合ribbon+openFeign+fallback

---
+++ cloudalibaba-provider-payment9003/src/main/java/cn/les/springcloud/PaymentMain9003.java
@@ -0,0 +1,21 @@
+package cn.les.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author
+ * @version v1.0.0
+ * @descripton
+ * @2020/12/26 14:18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+
+}
